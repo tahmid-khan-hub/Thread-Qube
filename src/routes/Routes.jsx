172cf52c014c5e59d4b42197ad3606ac7cb39e77
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-07-07 22:20:08 +0600

    packages installation

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createBrowserRouter } from "react-router";
+
+const Routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+export default Routes;
