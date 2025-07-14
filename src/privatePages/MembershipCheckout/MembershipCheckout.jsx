4723c7a78bffabfca6881000a0ba8e96139bb15b
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-07-15 02:28:58 +0600

    updated membership page

--- src/privatePages/MembershipCheckout/MembershipCheckout.jsx
+++ src/privatePages/MembershipCheckout/MembershipCheckout.jsx
@@ -6,8 +6,7 @@ import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../hooks/useAuth";
 import useAxiosSecure from "../../hooks/UseAxiosSecure";
 import Loader from "../../pages/Loader/Loader";
-import Lottie from "lottie-react";
-import AlreadyMemberLottie from "../../assets/lotties/AlreadyMember.json"
+
 
 const MembershipCheckout = () => {
   const { user } = useAuth();
@@ -28,7 +27,6 @@ const MembershipCheckout = () => {
   if (User?.badge === "gold") {
     return (
       <div className="min-h-screen flex flex-col items-center justify-center">
-        <Lottie animationData={AlreadyMemberLottie} className="w-52 h-52" />
         <div className="text-center mt-6">
           <h2 className="text-2xl font-semibold text-orange-600">
             🎉 You're already a Gold Member!
