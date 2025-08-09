3a55eaae85893299a83005a1e1b3ca8a4fc3c137
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-08-09 20:01:53 +0600

    updated footer

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import brand from "../../assets/logo.png";
 import { Link } from "react-router";
+import useAxiosSecure from "../../hooks/UseAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import Loader from "../../pages/Loader/Loader";
 
 const Footer = () => {
+  const axiosSecure = useAxiosSecure();
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["terms"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/staticPages/social");
+      return res.data;
+    },
+  });
+
+  const facebookLink = data.facebook;
+  const twitterLink = data.twitter;
+  const linkedinLink = data.linkedin;
+
+  if(isLoading) return <Loader></Loader>;
+
   return (
     <footer className="bg-orange-100 text-black">
       <div className="max-w-[1400px] w-[96%] mx-auto px-3 lg:px-4 py-6 md:flex md:items-center md:justify-between">
@@ -25,17 +44,17 @@ const Footer = () => {
 
         {/* Right: Social Icons */}
         <div className="flex space-x-4">
-          <a href="https://facebook.com" target="_blank" className="hover:text-blue-500">
+          <a href={facebookLink} target="_blank" className="hover:text-blue-500">
             <svg fill="currentColor" className="w-4 h-4" viewBox="0 0 24 24">
               <path d="M22 12a10 10 0 1 0-11.5 9.9v-7h-2v-3h2v-2c0-2 1-3 3-3h2v3h-2c-.5 0-1 .5-1 1v2h3l-.5 3h-2.5v7A10 10 0 0 0 22 12z" />
             </svg>
           </a>
-          <a href="https://twitter.com" target="_blank" className="hover:text-sky-400">
+          <a href={twitterLink} target="_blank" className="hover:text-sky-400">
             <svg fill="currentColor" className="w-4 h-4" viewBox="0 0 24 24">
               <path d="M23 3a10.9 10.9 0 0 1-3 1 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
             </svg>
           </a>
-          <a href="https://linkedin.com" target="_blank" className="hover:text-blue-600">
+          <a href={linkedinLink} target="_blank" className="hover:text-blue-600">
             <svg fill="currentColor" className="w-4 h-4" viewBox="0 0 24 24">
               <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-4 0v7h-4v-7a6 6 0 0 1 6-6zM2 9h4v12H2zM4 2a2 2 0 1 1 0 4 2 2 0 0 1 0-4z" />
             </svg>
